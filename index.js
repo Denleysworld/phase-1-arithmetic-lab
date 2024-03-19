537d1548ef8b86c16637f268c738afb1d00a974c
Denleysworld <deekayel05@gmail.com>
2024-03-19 13:27:06 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 31
+const num2 =2
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 100) + 1;
+let max= Math.max(1,2,3,4,20)
+const num3 = 16
+const num4=4
+let mod=num3/num4
